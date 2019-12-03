749d6b952be3053aad1e95f2ab1675109d01db81
Raymond Robert Price <darthbob88@gmail.com>
2019-12-03 13:46:26 -0800

    Added some more receipts

--- src/receiptData.ts
+++ src/receiptData.ts
@@ -54,5 +54,79 @@ export default [
       { name: "Companion Cube (x1)", price: 200 },
       { name: "Cake (x0)", price: 0 }
     ]
+  },
+  {
+    storeName: "Belethor's Supplies",
+    location: "Whiterun Hold, Whiterun",
+    date: "06/26/2012",
+    currency: "septims x",
+    items: [
+      { name: "Cabbages (x5)", price: 50 },
+      { name: "Steel Dagger (x1)", price: 100 },
+      { name: "Steel Helmet (x1)", price: 200 }
+    ]
+  },
+  {
+    storeName: "Ammu-nation",
+    location: "Liberty City, USA",
+    date: "10/22/2001",
+    currency: "$",
+    items: [
+      { name: "Micro Uzi (x1)", price: 1000 },
+      { name: "Sniper Rifle (x1)", price: 800 },
+      { name: "Body Armor (x1)", price: 2000 }
+    ]
+  },
+  {
+    storeName: "Hylian Bazaar",
+    location: "Town Marketplace, Hyrule Castle",
+    date: "11/21/1988",
+    currency: "₹",
+    items: [{ name: "Hylian Shield (x1)", price: 1000 }]
+  },
+  {
+    storeName: "Trusty Patches",
+    location: "Catacombs, Firelink Shrine",
+    date: "09/22/2011",
+    currency: "souls x",
+    items: [
+      { name: "Humanity (x1)", price: 10000 },
+      { name: "Mask of the Father (x1)", price: 8000 },
+      { name: "Estus Flask (x1)", price: 1000 }
+    ]
+  },
+  {
+    storeName: "Steve's Supplies",
+    location: "Gravel Road, Village",
+    date: "05/17/2009",
+    currency: "emeralds x",
+    items: [
+      { name: "Steel Pickaxe (x1)", price: 400 },
+      { name: "Diamond Sword (x1)", price: 1000 },
+      { name: "Redstone Block (x10)", price: 1000 }
+    ]
+  },
+  {
+    storeName: "Craterside Supply",
+    location: "Megaton, Capital Wasteland",
+    date: "10/31/2008",
+    currency: "caps x",
+    items: [
+      { name: "Bottlecap Mine (x1)", price: 400 },
+      { name: "Vault Suit (x1)", price: 200 },
+      { name: "Fat Man (x1)", price: 1000 }
+    ]
+  },
+  {
+    storeName: "The Smuggler",
+    location: "Hell's Kitchen, NYC",
+    date: "06/23/2000",
+    currency: "$",
+    items: [
+      { name: "Lockpicks (x10)", price: 100 },
+      { name: "Multitool (x10)", price: 200 },
+      { name: "10mm cartridges (x300)", price: 150 },
+      { name: "Thermoptic Camo (x1)", price: 1000 }
+    ]
   }
 ];
